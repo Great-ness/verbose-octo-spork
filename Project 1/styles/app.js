9b2daccaba02921f3573aeb1f9ce2e31b3b67b80
ajayidgreat <99724085+ajayidgreat@users.noreply.github.com>
2022-02-15 21:15:19 +0100

    Add files via upload

---
+++ Project 1/styles/app.js
@@ -0,0 +1,46 @@
+// let name ="Chris" //String
+// let age = 30
+// let itsRaining = true
+// let color = undefined
+// let lastname = null
+
+let name ="Jeff";
+let surname ="Brad";
+let date =15;
+let dates =16;
+let dontFall = true;
+let pleaseFall= false;
+let weather = undefined;
+let firstname = null;
+console.log (surname)
+
+
+let person ={
+    name:"Dave",
+    age:35,
+    address:{
+        state:"Lagos",
+        city:"Ikeja",
+        street:"42 Adeleke Close",
+    },
+    hobbies:["Swimming", "Partying", "Football"]
+};
+person.name ="James",
+person.age = 25,
+person.height ="6feet"
+console.log (person)
+
+let human ={
+    name:"John",
+    age: 27,
+    institution: {
+        school:"ful",
+        faculty:"Computer Science",
+        department:"javascript",
+    },
+    courses:["html", "css", "javascript"]
+};
+human["age"] =74,
+console.log(human.institution["faculty"]),
+console.log(human.courses[0]),
+console.log (human)
